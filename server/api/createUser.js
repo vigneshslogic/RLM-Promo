a24dddabbb7af1a699b3125d655864ecffe48cc0
TS-GeekDev <ts.geek04@gmail.com>
2024-10-04 18:33:55 +0530

    create new user account and customer

--- server/api/createUser.js
+++ server/api/createUser.js
@@ -6,21 +6,20 @@ export default defineEventHandler(async (event) => {
   try {
     const instanceURL = 'https://enterprise-velocity-2370-dev-ed.scratch.my.salesforce.com/services/data/v58.0/sobjects';
 
-    // const accountData = {
-    //   Name: 'Dev-testing',
-    //   Type: 'Customer', // Optional: specify account type if needed
-    // };
-
-    // const accountResponse = await axios.post(`${instanceURL}/account/`, accountData, {
-    //   headers: {
-    //     Authorization: `Bearer ${body.accessToken}`,
-    //     'Content-Type': 'application/json',
-    //   },
-    // });
-
     if (body.user) {
-      // body.contactData.AccountId = accountResponse.data?.id; // Associate contact with the newly created account
-      body.user.AccountId = '001Pv00000XN9R1IAL';
+      const accountData = {
+        Name: `${body?.user?.FirstName}-${body?.user?.LastName}`,
+        Type: 'Customer',
+      };
+
+      const accountResponse = await axios.post(`${instanceURL}/account/`, accountData, {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      body.contactData.AccountId = accountResponse.data?.id; // Associate contact with the newly created account
 
       const contactResponse = await axios.post(`${instanceURL}/Contact/`, body.user, {
         headers: {
