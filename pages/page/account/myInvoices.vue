58ff3b713315d211778467e376712866a991c5e4
TS-GeekDev <ts.geek04@gmail.com>
2024-10-14 15:12:47 +0530

    remove contact number field form order listings Add missing invoices fields

--- pages/page/account/myInvoices.vue
+++ pages/page/account/myInvoices.vue
@@ -7,8 +7,8 @@
         <div class="welcome-msg">
           <p>Hello, {{ user?.FirstName }} {{  user?.LastName }}</p>
           <p>
-            From your Orders you have the ability to view your all orders and
-            status of order.
+            From your Invoices you have the ability to view your all invoices and
+            status of invoice.
           </p>
         </div>
         <div class="box-account box-info">
@@ -17,23 +17,36 @@
               <div class="row mx-auto">
                 <div v-if="invoices.length">
                     <div class="card my-3" v-for="(item, index) in invoices" :key="index">
-                    <div class="card-header">
+                    <div class="card-header d-flex justify-content-between">
                       <div>
-                        <span class="fw-bold pe-3">Order No:</span>
+                        <span class="fw-bold pe-3">Invoice Document No:</span>
                         <span>{{ item?.DocumentNumber }}</span> 
                       </div>
+                      <button class="btn btn-solid btn-sm p-1">Pay Now</button>
                     </div>
                     <div class="card-body">
-                        <div class="row">
+                      <div class="row">
                         <div class="col d-flex flex-column">
-                            <span class="fw-bold">Date</span>
+                            <span class="fw-bold">Invoice Date</span>
                             <span>{{ item?.InvoiceDate }}</span>
+                        </div>
+                        <div class="col d-flex flex-column">
+                            <span class="fw-bold">Invoice Due Date</span>
+                            <span>{{ item?.DueDate }}</span>
+                        </div>
+                        <div class="col d-flex flex-column">
+                            <span class="fw-bold">Total Change Amount with Tax</span>
+                            <span>{{ item?.TotalChargeAmountWithTax }}</span>
+                        </div>
+                        <div class="col d-flex flex-column">
+                            <span class="fw-bold">Balance</span>
+                            <span>£&nbsp;&nbsp;{{ item?.Balance }}</span>
                         </div>
                         <div class="col d-flex flex-column">
                             <span class="fw-bold">Total Amount</span>
                             <span>£&nbsp;&nbsp;{{ Number(item?.TotalChargeAmountWithTax?.toFixed(2)) ?? 0 }}</span>
                         </div>
-                        </div>
+                      </div>
                     </div>
                     </div>
                 </div>
