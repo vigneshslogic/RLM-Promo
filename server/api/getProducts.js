7ece3f31c1329d85578dfe9bde0734d6f3d8017e
vigneshforcecrm <vigneshforcecrm@gmail.com>
2025-09-11 23:02:23 +0530

    feat: product-filter

--- server/api/getProducts.js
+++ server/api/getProducts.js
@@ -1,52 +1,54 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event); 
+  const body = await readBody(event);
   const config = useRuntimeConfig();
 
   try {
-    const url = `${config?.api_endpoint}/services/data/v${parseFloat(config?.api_version).toFixed(1)}/connect/cpq/products`;
-    let payload = {};
+    const url = `${config?.api_endpoint}/services/data/v${parseFloat(
+      config?.api_version
+    ).toFixed(1)}/connect/cpq/products`;
     
+    let payload = {
+      correlationId: 'corrId',
+      priceBookId: `${config?.pricebook_id}`,
+      additionalFields: {
+        Product2: {
+          fields: ["IsActive", "Show_in_Webshop__c"]
+        }
+      }
+    };
+
+
     if (!body?.isCategory && body?.catalogId) {
-      payload = {
-        'correlationId': 'corrId',
-        'catalogId': body?.catalogId,
-        'priceBookId': `${config?.pricebook_id}`,
-      } 
+      payload.catalogId = body.catalogId;
     } else if (body?.isCategory && body?.catalogId) {
-      payload = {
-        'correlationId': 'corrId',
-        'categoryId': body?.catalogId,
-        'priceBookId': `${config?.pricebook_id}`,
-      } 
-    } else {
-      payload = {
-        'correlationId': 'corrId',
-        'priceBookId': `${config?.pricebook_id}`,
-      } 
+      payload.categoryId = body.catalogId;
     }
 
-    console.log('Data-->',JSON.stringify(payload));
-    
     const response = await axios.post(url, payload, {
       headers: {
         Authorization: `Bearer ${body.accessToken}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.status === 200) {
-      response.data.result = response.data.result.filter((prod) => prod.isActive);
+      response.data.result = response.data.result.filter(
+          (prod) =>
+            prod?.additionalFields?.IsActive === true &&
+            prod?.additionalFields?.Show_in_Webshop__c === true
+        );
       return response.data;
     }
-    
-    return false;
 
+    return false;
   } catch (error) {
     throw createError({
       statusCode: error.response ? error.response.status : 500,
-      message: error.response ? error.response.data.error_description : error.message,
+      message: error.response
+        ? error.response.data.error_description
+        : error.message,
     });
   }
 });
